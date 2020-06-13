62df580cadfc5d05203300516b6eabbf627e260a
Philippe Fonzin <pfonzin@gmail.com>
2020-06-13 15:58:29 -0400

    Create todos component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './App.css';
+import Todos from "./components/Todos"; // importing Todos form components dir 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+// Different components can have their own state, but alot of times your have state 
+// that multiple components need to access. Todos need go in a place where we can feed it 
+// down to different components. Which is why we put our "Todos" stae into the main app 
+// component state. 
+
+// Next we want to take the todos that are in the main App component state below and 
+// pass them down into the todos component as a prop. The way we pass props is similar 
+// to an HTML attribute. Now, the state that we passed down as props to the embeded
+// "Todos" component is accessible within the "Todos" component.
+
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false 
+      },
+      {
+        id: 2,
+        title: "Dinner with shorty",
+        completed: false 
+      },
+      {
+        id: 3,
+        title: "Meeting with dude",
+        completed: false 
+      }
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Todos todos={this.state.todos}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+// React components let you split the UI into independent, reusable pieces, and think 
+// about each piece in isolation. 
+
+import React from "react"; // importing the react library 
+
+class Todos extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Todos</h1>
+      </div>
+    )
+  }
+}
+
+export default Todos; 
