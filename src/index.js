becfd3014d528219f73bae139ad7965064d01445
Philippe Fonzin <pfonzin@gmail.com>
2020-10-20 11:26:35 -0400

    clean up files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// StrictMode is a tool for highlighting potential problems in an application.
+// Like Fragment, StrictMode does not render any visible UI.It activates additional 
+// checks and warnings for its descendants.
+
+// StrictMode currently helps with:
+//   - Identifying components with unsafe lifecycles
+//   - Warning about legacy string ref API usage
+//   - Warning about deprecated findDOMNode usage
+//   - Detecting unexpected side effects
+//   - Detecting legacy context API
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
